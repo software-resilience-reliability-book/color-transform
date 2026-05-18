de6f985258da02f6690a1573072419cd949f50c2
Mike Jovanovich <mpjovanovich@gmail.com>
2026-05-18 10:35:48 -0400

    Updated test harness / sandbox to show how it can be used to step into library

--- tests/ColorTransform.Harness/Program.cs
+++ tests/ColorTransform.Harness/Program.cs
@@ -1,6 +1,13 @@
 ﻿using ColorTransform.Models;
+using ColorTransform.Utilities;
 
-var parser
 
-// var palette = new ColorPalette(new List<RgbColor> { new RgbColor(0, 0, 0) }, "Test");
-// Console.WriteLine(palette.Name);
+// Sandbox development test: use this to step into "FromHexString"
+var converter = new HexConverter();
+RgbColor color = converter.FromHexString("#FF5733");
+Console.WriteLine($"Red: {color.Red}, Green: {color.Green}, Blue: {color.Blue}");
+
+// Sandbox development test: use this to step into "ToHexString"
+RgbColor color2 = new RgbColor(255, 87, 51);
+string hex = converter.ToHexString(color2);
+Console.WriteLine($"Hex: {hex}");
